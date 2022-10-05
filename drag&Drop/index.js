853e48f038661e5ce1aca8521047398dfe9a66c6
Samuel <samuel_vr90@hotmail.com>
2022-10-05 19:58:22 +0200

    Lesson 16 (Drag & Drop)

--- drag&Drop/index.js
+++ drag&Drop/index.js
@@ -49,4 +49,4 @@ paperbin.addEventListener("drop", event => {
 - Añade dos párrafos más en cada una de las secciones
 - Pon un icono de papelera para eliminar párrafos (Esta imagen puede valer:
 https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSMWeEC0tULzOjfWFAOdOHjUw9WjXhBDbirg&usqp=CAU)
-- Añade el código necesario para que al arrastrar un fragmento del puzzle a la papelera, éste se elimine*/
+- Añade el código necesario para que al arrastrar una sección a la papelera, ésta se elimine*/
